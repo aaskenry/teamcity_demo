bcd595f2c72df65419fd676db54f0798f681ae3c
Anders Askenryd <Anders Askenryd anders.askenryd@gmail.com>
2017-06-15 00:52:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamCityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
